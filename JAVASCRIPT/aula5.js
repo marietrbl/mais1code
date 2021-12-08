0691dc90bf9e1a9f33f59ecde971e7ad5dd9f18d
Maria Clara <maria.claramtz94@gmail.com>
2021-12-07 21:13:11 -0300

    desafio corrigido

---
+++ JAVASCRIPT/aula5.js
@@ -0,0 +1,14 @@
+    function equacao(a,b,c){
+      var a = 1;
+      var b = 5;
+      var c = 6;
+
+        var delta= Math.pow(b,2)-(4*a*c)
+        var x1= (-b + Math.sqrt(delta))/(2*a); 
+        var x2= (-b - Math.sqrt(delta))/(2*a);         
+      }
+
+
+      console.log(x1);
+      console.log(x2); 
+  
